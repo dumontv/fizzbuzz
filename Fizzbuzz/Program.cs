e61a79bcd1301d3c6b5342d5d15de52dbbb42f0b
Vincent <dumontv@outlook.com>
2017-02-07 13:04:18 -0500

    fizzbuzzing

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type how many numbers you want:");
+            int printCount = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            for (int i = 1; i <= printCount; i++)
+            {
+                string textToDisplay = $"{i} ";
+                if (i % 3 == 0)
+                {
+                    textToDisplay += "fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    textToDisplay += "buzz";
+                }
+                Console.Write(textToDisplay += "\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
